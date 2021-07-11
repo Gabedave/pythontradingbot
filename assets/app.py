228c5ad816829e6fc39f265198daa512e56d86f8
Gabedave <60360366+Gabedave@users.noreply.github.com>
2021-07-11 17:19:08 +0100

    Update app.py

--- assets/app.py
+++ assets/app.py
@@ -32,7 +32,7 @@ def home():
 def stream():
    if platform.system() == 'Windows':
       gen = execute_gen(['pythonpath.bat', '.', 'iqoptionbot/starter.py'])
-   else: gen = execute_gen(['PYTHONPATH=.', 'python', 'iqoptionbot/starter.py', 'somecommand'])
+   else: gen = execute_gen(['PYTHONPATH=$PWD', 'python', 'iqoptionbot/starter.py', 'somecommand'])
    return Response(stream_with_context(gen))
 
 @app.route('/trader/')
